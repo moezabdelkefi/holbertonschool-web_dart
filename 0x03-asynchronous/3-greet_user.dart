7f5973d2008fc68c823a1413dc4b383202473549
moezabdelkef <moezbadelkefi17@gmail.com>
2024-04-06 02:07:01 +0100

    Greet User

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import '3-util.dart';
+
+Future<String> greetUser() async {
+  try {
+    final userData = await fetchUserData();
+    final decodedData = jsonDecode(userData);
+    final username = decodedData['username'];
+    return 'Hello $username';
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    final credentials = await checkCredentials();
+    if (credentials) {
+      print('There is a user: true');
+    } else {
+      print('There is a user: false');
+      return 'Wrong credentials';
+    }
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
